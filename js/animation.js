5bc946273365767b63653c1774e1b83ceddcfa28
Lavanya3003 <lavanya@uxmint.in>
2020-09-30 15:59:33 +0530

    animation - icons

--- js/animation.js
+++ js/animation.js
@@ -14,13 +14,6 @@ new ScrollMagic.Scene({
 }).setClassToggle('.section-banner', 'kenburns-top').addTo(controller);
 
 new ScrollMagic.Scene({
-    triggerElement: '.icons-usability-methods',
-    offset: -100,
-    triggerHook: 1,
-    reverse:false
-}).setClassToggle('.icons-usability-methods', 'slide-top').addTo(controller);
-
-new ScrollMagic.Scene({
     triggerElement: '.banner-animation',
     offset: -50,
     triggerHook: 2.4,
@@ -71,3 +64,91 @@ new ScrollMagic.Scene({
     triggerHook: 0.6,
     reverse:false
 }).setClassToggle('.logo6', 'slide-top').addTo(controller);
+
+// Usability Testing 5 Icons 
+new ScrollMagic.Scene({
+    triggerElement: '.icon1',
+    offset: -100,
+    triggerHook: 1.6,
+    reverse:false
+}).setClassToggle('.icon1', 'slide-top').addTo(controller);
+new ScrollMagic.Scene({
+    triggerElement: '.icon2',
+    offset: -100,
+    triggerHook: 1.4,
+    reverse:false
+}).setClassToggle('.icon2', 'slide-top').addTo(controller);
+new ScrollMagic.Scene({
+    triggerElement: '.icon3',
+    offset: -100,
+    triggerHook: 1.2,
+    reverse:false
+}).setClassToggle('.icon3', 'slide-top').addTo(controller);
+new ScrollMagic.Scene({
+    triggerElement: '.icon4',
+    offset: -100,
+    triggerHook: 1,
+    reverse:false
+}).setClassToggle('.icon4', 'slide-top').addTo(controller);
+new ScrollMagic.Scene({
+    triggerElement: '.icon5',
+    offset: -100,
+    triggerHook: 0.8,
+    reverse:false
+}).setClassToggle('.icon5', 'slide-top').addTo(controller);
+
+//Usability Testing 9 Dev-icons dev-icon1
+new ScrollMagic.Scene({
+    triggerElement: '.dev-icon1',
+    offset: -100,
+    triggerHook: 2.2,
+    reverse:false
+}).setClassToggle('.dev-icon1', 'slide-top').addTo(controller);
+new ScrollMagic.Scene({
+    triggerElement: '.dev-icon2',
+    offset: -100,
+    triggerHook: 2,
+    reverse:false
+}).setClassToggle('.dev-icon2', 'slide-top').addTo(controller);
+new ScrollMagic.Scene({
+    triggerElement: '.dev-icon3',
+    offset: -100,
+    triggerHook: 1.8,
+    reverse:false
+}).setClassToggle('.dev-icon3', 'slide-top').addTo(controller);
+new ScrollMagic.Scene({
+    triggerElement: '.dev-icon4',
+    offset: -100,
+    triggerHook: 1.6,
+    reverse:false
+}).setClassToggle('.dev-icon4', 'slide-top').addTo(controller);
+new ScrollMagic.Scene({
+    triggerElement: '.dev-icon5',
+    offset: -100,
+    triggerHook: 1.4,
+    reverse:false
+}).setClassToggle('.dev-icon5', 'slide-top').addTo(controller);
+new ScrollMagic.Scene({
+    triggerElement: '.dev-icon6',
+    offset: -100,
+    triggerHook: 1.2,
+    reverse:false
+}).setClassToggle('.dev-icon6', 'slide-top').addTo(controller);
+new ScrollMagic.Scene({
+    triggerElement: '.dev-icon7',
+    offset: -100,
+    triggerHook: 1,
+    reverse:false
+}).setClassToggle('.dev-icon7', 'slide-top').addTo(controller);
+new ScrollMagic.Scene({
+    triggerElement: '.dev-icon8',
+    offset: -100,
+    triggerHook: 0.9,
+    reverse:false
+}).setClassToggle('.dev-icon8', 'slide-top').addTo(controller);
+new ScrollMagic.Scene({
+    triggerElement: '.dev-icon9',
+    offset: -100,
+    triggerHook: 0.9,
+    reverse:false
+}).setClassToggle('.dev-icon9', 'slide-top').addTo(controller);
