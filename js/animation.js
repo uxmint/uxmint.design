a2203d51024ef3b2e0448252fbfbb985fa2e8103
Dhanvarsha147 <dhanvarsha@uxmint.in>
2020-09-30 14:59:17 +0530

    service - animation

--- js/animation.js
+++ js/animation.js
@@ -5,3 +5,19 @@ new ScrollMagic.Scene({
     triggerHook: 1,
     reverse:false
 }).setClassToggle('.case-study-grid', 'slide-top').addTo(controller);
+
+var controller = new ScrollMagic.Controller();
+new ScrollMagic.Scene({
+    triggerElement: '.section-banner',
+    offset: -100,
+    triggerHook: 1,
+    reverse:false
+}).setClassToggle('.section-banner', 'kenburns-top').addTo(controller);
+
+var controller = new ScrollMagic.Controller();
+new ScrollMagic.Scene({
+    triggerElement: '.icons-usability-methods',
+    offset: -100,
+    triggerHook: 1,
+    reverse:false
+}).setClassToggle('.icons-usability-methods', 'slide-top').addTo(controller);
