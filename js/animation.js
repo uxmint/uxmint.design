bf228e2687f39815b349a0db49b29b322b8e0041
Lavanya3003 <lavanya@uxmint.in>
2020-10-01 22:18:50 +0530

    UI fixes

--- js/animation.js
+++ js/animation.js
@@ -40,13 +40,6 @@ new ScrollMagic.Scene({
 }).setClassToggle('.card2', 'slide-top').addTo(controller);
 
 new ScrollMagic.Scene({
-    triggerElement: '.banner-animation',
-    offset: -50,
-    triggerHook: 2.4,
-    reverse:false
-}).setClassToggle('.banner-animation', 'slide-top').addTo(controller);
-
-new ScrollMagic.Scene({
     triggerElement: '.slide-image',
     offset: -100,
     triggerHook: 1,
@@ -94,7 +87,7 @@ new ScrollMagic.Scene({
 new ScrollMagic.Scene({
     triggerElement: '.logo6',
     offset: -100,
-    triggerHook: 0.6,
+    triggerHook: 0.8,
     reverse:false
 }).setClassToggle('.logo6', 'slide-top').addTo(controller);
 
