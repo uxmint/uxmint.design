f992f9344f8a68dd8df1f7888cd2873a339d0f2c
Lavanya3003 <lavanya@uxmint.in>
2021-09-07 15:50:37 +0530

    works

--- src/app/modules/work/work.module.ts
+++ src/app/modules/work/work.module.ts
@@ -5,13 +5,17 @@ import { WorkRoutingModule } from './work-routing.module';
 import { NetworkingComponent } from './components/networking/networking.component';
 import { EventManagementComponent } from './components/event-management/event-management.component';
 import { HealthCareComponent } from './components/health-care/health-care.component';
+import { NguCarouselModule } from '@ngu/carousel';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 
 @NgModule({
   declarations: [NetworkingComponent, EventManagementComponent, HealthCareComponent],
   imports: [
     CommonModule,
-    WorkRoutingModule
+    WorkRoutingModule,
+    NguCarouselModule,
+    NgbModule
   ]
 })
 export class WorkModule { }
