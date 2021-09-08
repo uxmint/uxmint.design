46b68b930636b22f9c55c66993cc9306d6a089c7
kamalipalanisamy <kamali@uxmint.in>
2021-09-08 16:26:28 +0530

    responsive

--- src/app/components/engagement-models/engagement-models.component.ts
+++ src/app/components/engagement-models/engagement-models.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EngagementModelsComponent implements OnInit {
   active = '1';
+  activeIds = 'static-1';
   constructor() { }
 
   ngOnInit(): void {
