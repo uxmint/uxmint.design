5422c09c6c09fd02989e71413401ab076bdafd69
Lavanya3003 <lavanya@uxmint.in>
2021-09-08 14:23:58 +0530

    Work fixes

---
+++ src/app/modules/work/components/childcare-portal/childcare-portal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-childcare-portal',
+  templateUrl: './childcare-portal.component.html',
+  styleUrls: ['./childcare-portal.component.scss']
+})
+export class ChildcarePortalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/modules/work/components/event-management/event-management.component.ts
+++ src/app/modules/work/components/event-management/event-management.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NguCarouselConfig } from '@ngu/carousel';
 
 @Component({
   selector: 'app-event-management',
@@ -6,6 +7,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./event-management.component.scss']
 })
 export class EventManagementComponent implements OnInit {
+  blogsCarouselConfig:NguCarouselConfig = {
+    grid: { xs: 1, sm: 1, md: 1, lg: 1, all: 0 },
+    slide: 1,
+    speed: 400,
+    interval: {
+      timing: 4000,
+      initialDelay: 1000
+    },
+    point: {
+      visible: true
+    },
+    load: 2,
+    touch: true,
+    loop: true
+  };
+
+  blogs:any[] = [
+    { 
+      img: './assets/images/services/image1.png',
+      title: 'Designing a successful product: Is not a daydream',
+      sub: 'by Udaya Kumar Sivagurunathan'
+    },
+    { 
+      img: './assets/images/services/image2.png',
+      title: 'Even thought why does the colour green or sound...',
+      sub: 'by Krithika Thirunavukkarasu'
+    },
+    { 
+      img: './assets/images/services/image1.png',
+      title: 'Designing a successful product: Is not a daydream',
+      sub: 'by Udaya Kumar Sivagurunathan'
+    }
+  ];
 
   constructor() { }
 
