9ba87abda2d3cd52313bb3befaca37ec595a0291
rajasekar-d <rsekar.developer@gmail.com>
2021-10-05 10:50:14 +0530

    Prod

---
+++ deploy-dev.js
@@ -0,0 +1,21 @@
+const ghpages = require('gh-pages');
+const fs = require('fs-extra');
+console.log('Publishing Please Wait...');
+if(!fs.existsSync('dist')){
+    fs.mkdirSync('dist');
+}
+fs.readdirSync('./').forEach(function(file){
+    if(file.endsWith('.html')){
+        fs.copyFileSync(file,`dist/${file}`);
+    }
+    if(['js','css','images'].includes(file)){
+       fs.copySync(file,`dist/${file}`);
+    }
+});
+ghpages.publish('./dist',{
+    dest: 'dev',
+    message: 'Auto-generated commit'
+},function(){
+    console.log('Successfully published! Have a nice day!');
+    fs.unlinkSync('dist');
+});
