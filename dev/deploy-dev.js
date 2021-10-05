9a6096164b71a20567092e6c455b391ad49e1f2b
rajasekar-d <rsekar.developer@gmail.com>
2021-10-05 09:48:10 +0530

    Auto-generated commit

--- dev/deploy-dev.js
+++ dev/deploy-dev.js
@@ -3,7 +3,7 @@ console.log('Publishing Please Wait...');
 ghpages.publish('./',{
     dest: 'dev',
     message: 'Auto-generated commit',
-    remove: ['node_modules','package-lock.json','package.json','deploy.js']
+    remove: ['dev/node_modules','dev/package-lock.json','dev/package.json','dev/deploy.js','dev/deploy-dev.js']
 },function(){
     console.log('Successfully published! Have a nice day!');
 });
