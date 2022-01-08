7ae731fda0878e113a11d0af369660fe09951d2b
rajasekar-d <rsekar.developer@gmail.com>
2022-01-08 20:47:39 +0530

    Added Deployment Script

---
+++ deploy-prod.js
@@ -0,0 +1,17 @@
+const ghpages = require('gh-pages');
+const fs = require('fs-extra');
+console.log('Publishing Please Wait...');
+const { parseAngularRoutes } = require('guess-parser');
+const routes = parseAngularRoutes('tsconfig.app.json');
+routes.filter(route=>route.path).forEach(route=>{
+    if(!fs.existsSync(`dist/uxmint${route.path}`)){
+        fs.mkdirSync(`dist/uxmint${route.path}`,{ recursive: true });
+    }
+    fs.copyFileSync('dist/uxmint/index.html',`dist/uxmint${route.path}/index.html`);
+});
+ghpages.publish('./dist/uxmint',{
+    message: 'Auto-generated commit'
+},function(){
+    console.log('Successfully published! Have a nice day!');
+    fs.unlinkSync('dist');
+});
