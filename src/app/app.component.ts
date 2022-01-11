bf10ba0eefe014598a7eeffe0e4def40c640548a
Dhanvarsha147 <dhanvarsha@uxmint.in>
2022-01-11 16:14:38 +0530

    Metatag

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { HeaderComponent } from './components/header/header.component';
+import { SeoService } from './services/seo.service';
+import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
+import { filter, map, mergeMap, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -10,12 +13,25 @@ import { HeaderComponent } from './components/header/header.component';
 export class AppComponent implements OnInit {
   title = 'uxmint';
 
-  constructor(private modalService: NgbModal){
+  constructor(private modalService: NgbModal, private router: Router, private activatedRoute: ActivatedRoute, private seoService: SeoService){
 
   }
 
   ngOnInit(){
-   
+    this.router.events.pipe(
+      filter(e => e instanceof NavigationEnd),
+      map(e => this.activatedRoute),
+      map((route) => {
+        while (route.firstChild) route = route.firstChild;
+        return route;
+      }),
+      filter((route) => route.outlet === 'primary'),
+      mergeMap((route) => route.data),
+    ).subscribe(data => {
+      let seoData = data['seo'];
+      this.seoService.updateTitle(seoData['title']);
+      this.seoService.updateMetaTags(seoData['metaTags']);
+    });
   }
 
   openMenu(){
